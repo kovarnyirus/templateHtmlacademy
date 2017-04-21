5a5f4a7f3dfbea3fdf14edf4578a0a843ce2b3b8
“kovarnyirus” <kovarnyi@gmail.com>
2017-04-21 12:46:22 +0300

    header

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ gulp.task("style", function() {
     .pipe(minify())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("css"))
-    .pipe(gulp.dest("build/css"))
+    /*.pipe(gulp.dest("build/css"))*/
     .pipe(server.stream());
 });
 
